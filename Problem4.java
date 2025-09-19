bab0f7809edfe209ff5cdca5170f51de3cef6833
Munnuru Manik Bharath <bharathmunnuru23@gmail.com>
2025-09-19 13:14:08 +0530

    multiplecounter

---
+++ Problem4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Problem4 {
+public static void main(String[] args) {
+	int [] arr= {1,2,8,9,12,46,76,82,15,20,30};
+	Map<Integer, Integer> countMap=new LinkedHashMap<>();
+	for(int i=1;i<=9;i++) {
+		int count=0;
+		for(int num: arr) {
+			if(num%i==0)
+				count++;
+			
+		}
+		countMap.put(i,  count);
+	}
+	System.out.println(countMap);
+}
+}
